489412b1e02557a408fc128eb630bf60b04a0ede
T Taylor <terrytaylorbonn@gmail.com>
2025-01-07 13:12:22 -0500

    gave up for now .. chaotic video

--- server.js
+++ server.js
@@ -2,15 +2,22 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
-const { logger } = require('./middleware/logEvents');
+// const logEvents = require('./middleware/logEvents');
+const { logger , logEvents } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
 
-// custom middleware logger
+// // custom middleware logger
 app.use(logger);
+// app.use((req, res, next) => {
+//     logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, 'reqLog.txt');
+//     console.log(`${req.method} ${req.path}`);
+//     next();
+// });
+
 
 // Cross Origin Resource Sharing
-const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
+const whitelist = ['https://www.google.com','http://www.google.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
 const corsOptions = {
     origin: (origin, callback) => {
         if (whitelist.indexOf(origin) !== -1 || !origin) {
@@ -22,6 +29,7 @@ const corsOptions = {
     optionsSuccessStatus: 200
 }
 app.use(cors(corsOptions));
+// app.use(cors());
 
 // built-in middleware to handle urlencoded data
 // in other words, form data:  
@@ -31,60 +39,37 @@ app.use(express.urlencoded({ extended: false }));
 // built-in middleware for json 
 app.use(express.json());
 
-//serve static files
+//serve stati c files
+// app.use('/', express.static(path.join(__dirname, '/public')));
 app.use(express.static(path.join(__dirname, '/public')));
+// app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
 app.get('^/$|/index(.html)?', (req, res) => {
     //res.sendFile('./views/index.html', { root: __dirname });
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-app.get('/new-page(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-});
-
-app.get('/old-page(.html)?', (req, res) => {
-    res.redirect(301, '/new-page.html'); //302 by default
-});
-
-// Route handlers
-app.get('/hello(.html)?', (req, res, next) => {
-    console.log('attempted to load hello.html');
-    next()
-}, (req, res) => {
-    res.send('Hello World!');
+app.get('/*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 
-// chaining route handlers
-const one = (req, res, next) => {
-    console.log('one');
-    next();
-}
+// // routes
+// app.use('/', require('./routes/root'));
+// app.use('/subdir', require('./routes/subdir'));
+// app.use('/employees', require('./routes/api/employees'));
 
-const two = (req, res, next) => {
-    console.log('two');
-    next();
-}
+// app.all('*', (req, res) => {
+//     res.status(404);
+//     if (req.accepts('html')) {
+//         res.sendFile(path.join(__dirname, 'views', '404.html'));
+//     } else if (req.accepts('json')) {
+//         res.json({ "error": "404 Not Found" });
+//     } else {
+//         res.type('txt').send("404 Not Found");
+//     }
+// });
 
-const three = (req, res) => {
-    console.log('three');
-    res.send('Finished!');
-}
-
-app.get('/chain(.html)?', [one, two, three]);
-
-app.all('*', (req, res) => {
-    res.status(404);
-    if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'));
-    } else if (req.accepts('json')) {
-        res.json({ "error": "404 Not Found" });
-    } else {
-        res.type('txt').send("404 Not Found");
-    }
-});
-
-app.use(errorHandler);
+// app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
