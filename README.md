849830afe16e96ff866894937faae1d3f4236157
T Taylor <terrytaylorbonn@gmail.com>
2025-01-07 11:39:45 -0500

    starter for ch2

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# "Introduction to Express JS tutorial"
+# "Express JS Tutorial - Middleware"
 
 ✅ [Check out my YouTube Channel with all of my tutorials](https://www.youtube.com/DaveGrayTeachesCode).
 
 **Description:**
 
-This repository shares the code applied during the Youtube tutorial. The tutorial is part of a [Node.js for Beginners Playlist](https://www.youtube.com/playlist?list=PL0Zuz27SZ-6PFkIxaJ6Xx_X46avTM1aYw) on my channel.  
+This repository shares the code applied during the Youtube tutorial. The tutorial is part of a [Node.js & Express for Beginners Playlist](https://www.youtube.com/playlist?list=PL0Zuz27SZ-6PFkIxaJ6Xx_X46avTM1aYw) on my channel.  
 
-[YouTube Tutorial](https://youtu.be/jivyItmsu18) for this repository.
+[YouTube Tutorial](https://youtu.be/y18ubz7gOsQ) for this repository.
 
 I suggest completing my [8 hour JavaScript course tutorial video](https://youtu.be/EfAl9bwzVZk) if you are new to Javascript.
 
--- server.js
+++ server.js
@@ -1,7 +1,38 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
+
+// custom middleware logger
+app.use(logger);
+
+// Cross Origin Resource Sharing
+const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+
+// built-in middleware to handle urlencoded data
+// in other words, form data:  
+// ‘content-type: application/x-www-form-urlencoded’
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json 
+app.use(express.json());
+
+//serve static files
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('^/$|/index(.html)?', (req, res) => {
     //res.sendFile('./views/index.html', { root: __dirname });
@@ -24,14 +55,6 @@ app.get('/hello(.html)?', (req, res, next) => {
     res.send('Hello World!');
 });
 
-// terry added @1830
-app.get('/hello2(.html)?', (req, res, next) => {
-    console.log('attempted to load hello2.html');
-    next()
-}, (req, res) => {
-    res.send('Hello World 2!');
-} );
-
 
 // chaining route handlers
 const one = (req, res, next) => {
@@ -51,8 +74,17 @@ const three = (req, res) => {
 
 app.get('/chain(.html)?', [one, two, three]);
 
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-})
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ "error": "404 Not Found" });
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
+});
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
