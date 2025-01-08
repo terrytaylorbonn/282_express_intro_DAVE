78305380cf5331d04884ceaf776d50682aa3a1d3
T Taylor <terrytaylorbonn@gmail.com>
2025-01-08 17:43:24 -0500

    25.0108 1743pm ch6

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -1,38 +1,17 @@
 const express = require('express');
 const router = express.Router();
-// const data = {};
-// data.employees = require('../../data/employees.json');
 const employeesController = require('../../controllers/employeesController');
-
+// const verifyJWT = require('../../middleware/verifyJWT');
 
 router.route('/')
     .get(employeesController.getAllEmployees)
     .post(employeesController.createNewEmployee)
     .put(employeesController.updateEmployee)
     .delete(employeesController.deleteEmployee)
-    // .get((req, res) => {
-    //     res.json(data.employees);
-    // })
-    // .post((req, res) => {
-    //     res.json({
-    //         "firstname": req.body.firstname,
-    //         "lastname": req.body.lastname
-    //     });
-    // })
-    // .put((req, res) => {
-    //     res.json({
-    //         "firstname": req.body.firstname,
-    //         "lastname": req.body.lastname
-    //     });
-    // })
-    // .delete((req, res) => {
-    //     res.json({ "id": req.body.id })
-    // });
+
 
 router.route('/:id')
     .get(employeesController.getEmployee)
-    // .get((req, res) => {
-    //     res.json({ "id": req.params.id });
-    // });
+
 
 module.exports = router;
